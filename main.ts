6c669fa70867cf495efce6b186e4f4fad6071a58
latrellBaptista <72996921+latrellBaptista@users.noreply.github.com>
2020-10-29 20:19:30 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,27 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    duck.vy = -60
+})
+let duck: Sprite = null
+duck = sprites.create(img`
+    . . . . . . . . . . b 5 b . . . 
+    . . . . . . . . . b 5 b . . . . 
+    . . . . . . b b b b b b . . . . 
+    . . . . . b b 5 5 5 5 5 b . . . 
+    . . . . b b 5 d 1 f 5 5 d f . . 
+    . . . . b 5 5 1 f f 5 d 4 c . . 
+    . . . . b 5 5 d f b d d 4 4 . . 
+    . b b b d 5 5 5 5 5 4 4 4 4 4 b 
+    b d d d b b d 5 5 4 4 4 4 4 b . 
+    b b d 5 5 5 b 5 5 5 5 5 5 b . . 
+    c d c 5 5 5 5 d 5 5 5 5 5 5 b . 
+    c b d c d 5 5 b 5 5 5 5 5 5 b . 
+    . c d d c c b d 5 5 5 5 5 d b . 
+    . . c b d d d d d 5 5 5 b b . . 
+    . . . c c c c c c c c b b . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+duck.setFlag(SpriteFlag.StayInScreen, true)
+scene.setBackgroundColor(9)
+effects.clouds.startScreenEffect()
+duck.y = 100
+duck.ay = 100
